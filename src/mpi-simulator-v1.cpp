6d40097e52876a214aa62f831eab0c7e70337cef
Makoto Tomokiyo <mtomokiy@andrew.cmu.edu>
2022-11-01 12:55:38 -0400

    deserializing particle list

--- src/mpi-simulator-v1.cpp
+++ src/mpi-simulator-v1.cpp
@@ -6,6 +6,8 @@
 #define COORDINATOR 0
 #define INT_TYPES_PER_PARTICLE 6 // 1 int, 1 float, 2x vec2 (2 floats)
 
+static int pid;
+
 void simulateStep(const QuadTree &quadTree,
                   const std::vector<Particle> &particles,
                   std::vector<Particle> &newParticles, StepParameters params) {
@@ -24,12 +26,53 @@ void simulateStep(const QuadTree &quadTree,
     }
 }
 
-void init_particle_list(const std::vector<Particle> &particles, int &particles) {
+/**
+ * particle list is
+ * | int (4) | float (4) | vec2: 2 floats (8) | vec2 (8) |
+*/
+void init_particle_list(const std::vector<Particle> &particles, int &num_particles) {
 
+  /* broadcast size of particle list */
+  MPI_Bcast(
+    &num_particles, 
+    1, 
+    MPI_INT, 
+    COORDINATOR, 
+    MPI_COMM_WORLD);
+
+  /* create particle array for broadcast */
+  uint32_t raw_particle_list[num_particles]; // all nodes
+
+  /* copy loaded particle data */
+  if (pid == COORDINATOR) {
+    memcpy(
+      (void *)&raw_particle_list[0], 
+      (void *)&particles[0], 
+      INT_TYPES_PER_PARTICLE * sizeof(int) * num_particles);
+  }
+
+  /* broadcast raw particle data */
+  MPI_Bcast(
+    &raw_particle_list[0], 
+    INT_TYPES_PER_PARTICLE * num_particles,
+    MPI_INT,
+    COORDINATOR,
+    MPI_COMM_WORLD);
+
+  /* satellite nodes unmarshal particle list */
+  if (pid != COORDINATOR) {
+    for (int i = 0; i < num_particles; i += INT_TYPES_PER_PARTICLE) {
+      Particle p;
+      float *float_fields = (float *)&raw_particle_list[i + 1];
+      p.id = raw_particle_list[i];
+      p.mass = float_fields[0];
+      p.position = Vec2(float_fields[1], float_fields[2]);
+      p.velocity = Vec2(float_fields[3], float_fields[4]);
+    }
+  }
 }
 
 int main(int argc, char *argv[]) {
-  int pid;
   int nproc;
   int len;
   int num_particles;
